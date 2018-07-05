08957ac9db0456d1ef5a4a17ab69ebeb0bfcc9e2
Philly Stafford <phillystafford@gmail.com>
2018-07-05 16:54:39 +0100

    3. Using shared components - Complete

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,7 @@
-import Header from "../comps/Header";
+import Layout from "../components/MyLayout.js";
 
 export default () => (
-  <div>
-    <Header />
+  <Layout>
     <p>This is the about page</p>
-  </div>
+  </Layout>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Header from "../comps/Header";
+import Layout from '../components/MyLayout.js'
 
 export default () => (
-  <div>
-    <Header />
-    <p>Hello Next.js</p>
-  </div>
-);
+    <Layout>
+       <p>Hello Next.js</p>
+    </Layout>
+)
